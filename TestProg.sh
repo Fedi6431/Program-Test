0257d9ba4afa122d826bdd993123f4202d5d79a5
Fedi6431 <102946457+Fedi6431@users.noreply.github.com>
2024-06-15 21:23:58 +0200

    Update TestProg.sh

--- TestProg.sh
+++ TestProg.sh
@@ -14,15 +14,15 @@ Select option
 echo $banner 
 read -p "--> " user_choice
 
-if [ $user_choice = "1" ] then
+if [ $user_choice = 1 ]; then
   git clone https://github.com/Fedi6431/LHE-Local_Host_Executer.git
   echo LHE installed succesfully!
-if [ $user_choice = "2" ] then
+elif [ $user_choice = 2 ]; then
     rm -rfv LHE-Local_Host_Executer
     git clone https://github.com/Fedi6431/LHE-Local_Host_Executer.git
-if [ $user_choice = "3" ] then
+elif [ $user_choice = 3 ]; then
     rm -rfv LHE-Local_Host_Executer
-if [ $user_choice = "4" ] then
+elif [ $user_choice = 4 ]; then
     exit 0
 fi
 done
