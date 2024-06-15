2c0b47f944cae1938225249689d04a8d052365c4
Fedi6431 <102946457+Fedi6431@users.noreply.github.com>
2024-06-15 22:10:45 +0200

    Update TestProg.sh

--- TestProg.sh
+++ TestProg.sh
@@ -1,46 +1,61 @@
 #!/bin/bash
 
-# Update the system and remove old packages
-apt update -y
-apt full-upgrade -y 
-apt autoremove -y
+banner="What are you using:
+1) Linux 
+2) Tremux
+"
 
-# Install OpenJDK
-apt install openjdk -y
-# Check the Java version of OpenJDK
-java -version
+echo $banner
 
-# change the directory to the main dir
-cd ~
-# Code that will go in the installer.sh
-cat > installer.sh <<EOF
+read -p "--> " usr_ch
 
-while :
-do
-  echo -e "Select option\n1) Install\n2) Update\n3) Remove\n4) Close"
-  read -p "--> " user_choice
+if [ usr_ch = 1 ]; then
+elif [ usr_ch = 1 ]; then
+  # Update the system and remove old packages
+  apt update -y
+  apt full-upgrade -y 
+  apt autoremove -y
   
-  if [ "\$user_choice" = 1 ]; then
-    git clone https://github.com/Fedi6431/LHE-Local_Host_Executer.git
-    echo -e "\\nLHE installed successfully!\\n"
-  elif [ "\$user_choice" = 2 ]; then
-      rm -rfv LHE-Local_Host_Executer
-      echo -e "\\nRemoved old LHE-Local_Host_Executer\\n"
+  # Install OpenJDK
+  apt install openjdk -y
+  # Check the Java version of OpenJDK
+  java -version
+  
+  # change the directory to the main dir
+  cd ~
+  # Code that will go in the installer.sh
+  cat > installer.sh <<EOF
+  while :
+  do
+    echo -e "Select option\n1) Install\n2) Update\n3) Remove\n4) Close"
+    read -p "--> " user_choice
+    
+    if [ "\$user_choice" = 1 ]; then
       git clone https://github.com/Fedi6431/LHE-Local_Host_Executer.git
-      echo -e "\\nUpdated git LHE-Local_Host_Executer!\\n"
-  elif [ "\$user_choice" = 3 ]; then
-      rm -rfv LHE-Local_Host_Executer
-      echo -e "\\nRemoved LHE-Local_Host_Executer successfully!\\n"
-  elif [ "\$user_choice" = 4 ]; then
-      break
-  else 
-    echo -e "\\nWrong option\\n"
-  fi
-  read -p "Press Enter to continue"
-  clear
-done
-EOF
+      echo -e "\\nLHE installed successfully!\\n"
+    elif [ "\$user_choice" = 2 ]; then
+        rm -rfv LHE-Local_Host_Executer
+        echo -e "\\nRemoved old LHE-Local_Host_Executer\\n"
+        git clone https://github.com/Fedi6431/LHE-Local_Host_Executer.git
+        echo -e "\\nUpdated git LHE-Local_Host_Executer!\\n"
+    elif [ "\$user_choice" = 3 ]; then
+        rm -rfv LHE-Local_Host_Executer
+        echo -e "\\nRemoved LHE-Local_Host_Executer successfully!\\n"
+    elif [ "\$user_choice" = 4 ]; then
+        break
+    else 
+      echo -e "\\nWrong option\\n"
+    fi
+    read -p "Press Enter to continue"
+    clear
+  done
+  EOF
+  
+  # Make the installer.sh executable and run it
+  chmod +x installer.sh
+  ./installer.sh
+  
+fi
 
-# Make the installer.sh executable and run it
-chmod +x installer.sh
-./installer.sh
+
+
