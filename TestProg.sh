ea9b619c60c0dc6443f71873f3c1f7673d15e087
Fedi6431 <102946457+Fedi6431@users.noreply.github.com>
2024-06-15 22:23:13 +0200

    Update TestProg.sh

--- TestProg.sh
+++ TestProg.sh
@@ -2,22 +2,69 @@
 
 banner="What are you using:
 1) Linux 
-2) Tremux
+2) Termux
 "
 
-echo $banner
+echo "$banner"
 
 read -p "--> " usr_ch
 
-if [ usr_ch = 1 ]; then
-elif [ usr_ch = 1 ]; then
+if [[ $usr_ch = 1 ]]; then
+  username=$USER
   # Update the system and remove old packages
-  apt update -y
-  apt full-upgrade -y 
-  apt autoremove -y
+  sudo apt update -y
+  sudo apt full-upgrade -y 
+  sudo apt autoremove -y
   
-  # Install OpenJDK
-  apt install openjdk -y
+  # Install OpenJDK and git
+  sudo apt install openjdk -y
+  sudo apt install git
+  # Check the Java version of OpenJDK
+  java -version
+  
+  # change the directory to the main dir
+  cd /home/$username/desktop
+  # Code that will go in the installer.sh
+  cat > installer.sh <<EOF
+  while :
+  do
+    echo -e "Select option\n1) Install\n2) Update\n3) Remove\n4) Close"
+    read -p "--> " user_choice
+    
+    if [ "\$user_choice" = 1 ]; then
+      git clone https://github.com/Fedi6431/LHE-Local_Host_Executer.git
+      echo -e "\\nLHE installed successfully!\\n"
+    elif [ "\$user_choice" = 2 ]; then
+        rm -rfv LHE-Local_Host_Executer
+        echo -e "\\nRemoved old LHE-Local_Host_Executer\\n"
+        git clone https://github.com/Fedi6431/LHE-Local_Host_Executer.git
+        echo -e "\\nUpdated git LHE-Local_Host_Executer!\\n"
+    elif [ "\$user_choice" = 3 ]; then
+        rm -rfv LHE-Local_Host_Executer
+        echo -e "\\nRemoved LHE-Local_Host_Executer successfully!\\n"
+    elif [ "\$user_choice" = 4 ]; then
+        break
+    else 
+      echo -e "\\nWrong option\\n"
+    fi
+    read -p "Press Enter to continue"
+    clear
+  done
+EOF
+  
+  # Make the installer.sh executable and run it
+  chmod +x installer.sh
+  ./installer.sh
+
+elif [[ $usr_ch = 2 ]]; then
+  # Update the system and remove old packages
+  pkg update -y
+  pkg full-upgrade -y 
+  pkg autoremove -y
+  
+  # Install OpenJDK and git
+  pkg install openjdk -y
+  pkg install git
   # Check the Java version of OpenJDK
   java -version
   
@@ -49,7 +96,7 @@ elif [ usr_ch = 1 ]; then
     read -p "Press Enter to continue"
     clear
   done
-  EOF
+EOF
   
   # Make the installer.sh executable and run it
   chmod +x installer.sh
