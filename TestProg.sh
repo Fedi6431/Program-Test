6b7b42dec999b99c672b3eb5839d77e267822458
Fedi6431 <102946457+Fedi6431@users.noreply.github.com>
2024-06-15 22:03:57 +0200

    Update TestProg.sh

--- TestProg.sh
+++ TestProg.sh
@@ -12,23 +12,25 @@ java -version
 
 # Code that will go in the installer.sh
 cat > installer.sh <<EOF
+cd ~
+
 while :
 do
   echo -e "Select option\n1) Install\n2) Update\n3) Remove\n4) Close"
   read -p "--> " user_choice
   
-  if [ \$user_choice = 1 ]; then
+  if [ "\$user_choice" = 1 ]; then
     git clone https://github.com/Fedi6431/LHE-Local_Host_Executer.git
     echo -e "\\nLHE installed successfully!\\n"
-  elif [ \$user_choice = 2 ]; then
+  elif [ "\$user_choice" = 2 ]; then
       rm -rfv LHE-Local_Host_Executer
       echo -e "\\nRemoved old LHE-Local_Host_Executer\\n"
       git clone https://github.com/Fedi6431/LHE-Local_Host_Executer.git
       echo -e "\\nUpdated git LHE-Local_Host_Executer!\\n"
-  elif [ \$user_choice = 3 ]; then
+  elif [ "\$user_choice" = 3 ]; then
       rm -rfv LHE-Local_Host_Executer
       echo -e "\\nRemoved LHE-Local_Host_Executer successfully!\\n"
-  elif [ \$user_choice = 4 ]; then
+  elif [ "\$user_choice" = 4 ]; then
       break
   else 
     echo -e "\\nWrong option\\n"
@@ -37,9 +39,6 @@ do
   clear
 done
 EOF
-
-# Go to the main directory
-cd ~
 
 # Make the installer.sh executable and run it
 chmod +x installer.sh
