0172f931ddd4c1cee8b1f59f29863293ef54833e
Fedi6431 <102946457+Fedi6431@users.noreply.github.com>
2024-06-15 21:13:11 +0200

    Update and rename TestProg.py to TestProg.sh

---
+++ TestProg.sh
@@ -0,0 +1,19 @@
+banner='''
+Select option
+1) Install
+2) Update
+3) Remove
+4) Close
+'''
+echo $banner 
+read -p "--> " user_choice
+
+if [$user_choice="1"] then
+  git clone https://github.com/Fedi6431/LHE-Local_Host_Executer.git
+  echo LHE installed succesfully!
+elif [$user_choice="2"] then
+    rm -rfv LHE-Local_Host_Executer
+    git clone https://github.com/Fedi6431/LHE-Local_Host_Executer.git
+elif [$user_choice="3"] then
+    rm -rfv LHE-Local_Host_Executer
+fi
