c907080006bf7a78986c31e1140a87eee32680b6
Fedi6431 <102946457+Fedi6431@users.noreply.github.com>
2024-06-15 22:01:08 +0200

    Update TestProg.sh

--- TestProg.sh
+++ TestProg.sh
@@ -1,52 +1,46 @@
 #!/bin/bash
 
-# Update the system and remove old pkg
+# Update the system and remove old packages
 apt update -y
 apt full-upgrade -y 
 apt autoremove -y
 
-# install openjdk
-pkg install openjdk -y
-# try to take the java version of the openjdk
+# Install OpenJDK
+apt install openjdk-11-jdk -y
+# Check the Java version of OpenJDK
 java -version
 
-# code that will go in the installer.sh
-code ='''
+# Code that will go in the installer.sh
+cat > installer.sh <<EOF
 while :
 do
   echo -e "Select option\n1) Install\n2) Update\n3) Remove\n4) Close"
   read -p "--> " user_choice
   
-  if [ $user_choice = 1 ]; then
+  if [ \$user_choice = 1 ]; then
     git clone https://github.com/Fedi6431/LHE-Local_Host_Executer.git
-    echo -e "\nLHE installed succesfully!\n"
-  elif [ $user_choice = 2 ]; then
+    echo -e "\\nLHE installed successfully!\\n"
+  elif [ \$user_choice = 2 ]; then
       rm -rfv LHE-Local_Host_Executer
-      echo -e "\nRemoved old LHE-Local_Host_Executer\n"
+      echo -e "\\nRemoved old LHE-Local_Host_Executer\\n"
       git clone https://github.com/Fedi6431/LHE-Local_Host_Executer.git
-      echo -e "\nUpdated git LHE-Local_Host_Executer!\n"
-  elif [ $user_choice = 3 ]; then
+      echo -e "\\nUpdated git LHE-Local_Host_Executer!\\n"
+  elif [ \$user_choice = 3 ]; then
       rm -rfv LHE-Local_Host_Executer
-      echo -e "\nRemoved LHE-Local_Host_Executer succesfully!\n"
-  elif [ $user_choice = 4 ]; then
+      echo -e "\\nRemoved LHE-Local_Host_Executer successfully!\\n"
+  elif [ \$user_choice = 4 ]; then
       break
   else 
-    echo -e "\nWrong option\n"
+    echo -e "\\nWrong option\\n"
   fi
   read -p "Press Enter to continue"
   clear
 done
-'''
+EOF
 
-# go in the main direcotry
+# Go to the main directory
 cd ~
 
-# create the file installer.sh
-cat > installer.sh
-
-# inject the code in the installer.sh
-echo $code > installer.sh
-
-# start the installer.sh
+# Make the installer.sh executable and run it
 chmod +x installer.sh
-bash installer.sh
+./installer.sh
