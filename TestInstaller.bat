863a7513be7199c52513f72775abe508d1256988
Fedi6431 <102946457+Fedi6431@users.noreply.github.com>
2024-06-16 14:22:46 +0200

    Update TestInstaller.bat

--- TestInstaller.bat
+++ TestInstaller.bat
@@ -1,5 +1,42 @@
 @echo off
-https://github.com/git-for-windows/git/releases/download/v2.45.2.windows.1/Git-2.45.2-64-bit.exe
+
+rem Check if OpenJDK is installed
+java -version >nul 2>&1
+if %errorlevel% == 0 (
+    echo OpenJDK is already installed
+) else (
+    rem Install OpenJDK
+    set url=https://download.oracle.com/java/22/latest/jdk-22_windows-x64_bin.exe
+    set output=C:\Users\%username%\Desktop\jdk-22_windows-x64_bin.exe
+    (
+        echo $url = '%url%'
+        echo $output = '%output%'
+        echo Invoke-WebRequest -Uri $url -OutFile $output
+    ) > jdk_installer.ps1
+    powershell.exe -ExecutionPolicy Bypass -File jdk_installer.ps1
+    del jdk_installer.ps1
+    start C:\Users\%username%\Desktop\jdk-22_windows-x64_bin.exe
+    set /p WAIT="If OpenJDK is installed, press enter"
+)
+
+rem Check if Git is installed
+git --version >nul 2>&1
+if %errorlevel% == 0 (
+    echo Git is already installed
+) else (
+    rem Install Git
+    set url=https://github.com/git-for-windows/git/releases/download/v2.45.2.windows.1/Git-2.45.2-64-bit.exe
+    set output=C:\Users\%username%\Desktop\Git-2.45.2-64-bit.exe
+    (
+        echo $url = '%url%'
+        echo $output = '%output%'
+        echo Invoke-WebRequest -Uri $url -OutFile $output
+    ) > git_installer.ps1
+    powershell.exe -ExecutionPolicy Bypass -File git_installer.ps1
+    del git_installer.ps1
+    start C:\Users\%username%\Desktop\Git-2.45.2-64-bit.exe
+    set /p WAIT="If Git is installed, press enter"
+)
 
 :menu
 echo Installer menu
@@ -11,21 +48,11 @@ goto menu
 set /P user_choice="--> "
 
 if user_choice=="1" (
-  set url=https://download.oracle.com/java/22/latest/jdk-22_windows-x64_bin.exe
-  set output=C:\Users\%username%\Desktop\jdk-22_windows-x64_bin.exe
-(    
-    echo $url = '%url%'
-    echo $output = '%output%'
-    echo Invoke-WebRequest -Uri $url -OutFile $output
-  ) > installer.ps1
-  
-  powershell.exe -ExecutionPolicy Bypass -File download.ps1
-  
-  del installer.ps1
-  
-  start C:\Users\%username%\Desktop\jdk-22_windows-x64_bin.exe
-  
+
   )
 if user_choice=="2" (
   
+)
+if user_choice=="3" (
+
 )
