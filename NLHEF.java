df48c51f4729c8e72d0acbf3870bfbc4900b272c
Fedi6431 <102946457+Fedi6431@users.noreply.github.com>
2024-06-24 19:35:19 +0200

    Update NLHEF.java

--- NLHEF.java
+++ NLHEF.java
@@ -4,7 +4,7 @@ import java.io.InputStreamReader;
 public class CommandExecution {
     public static void main(String[] args) {
         try {
-            Process cmd_process = Runtime.getRuntime().exec(new String[]{"apt", "update"});
+            Process cmd_process = Runtime.getRuntime().exec(new String[]{"pkg", "update"});
             String line;
             BufferedReader input = new BufferedReader(new InputStreamReader(cmd_process.getInputStream()));
 
